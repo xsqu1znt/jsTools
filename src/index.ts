64f6115cd481d5443a4e353b08325099696179ce
squ1dggly <fluffyspiderstudios@gmail.com>
2025-02-13 16:38:49 -1000

    init

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import * as __async from "./async";
+import * as __array from "./array";
+import * as __date from "./date";
+import * as __file from "./file";
+import * as __number from "./number";
+import * as __object from "./object";
+import * as __random from "./random";
+import * as __string from "./string";
+
+export default {
+    ...__async,
+    ...__array,
+    ...__date,
+    ...__file,
+    ...__number,
+    ...__object,
+    ...__random,
+    ...__string
+};
